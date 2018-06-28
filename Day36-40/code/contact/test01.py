eade73fdea31bb12fdb68352266d89e0c5b83526
jackfrued <jackfrued@126.com>
2018-06-28 11:54:20 +0800

    '添加了PyMySQL的参考代码'

---
+++ Day36-40/code/contact/test01.py
@@ -0,0 +1,8 @@
+def main():
+    list1 = ['orange', 'grape', 'banana', 'waxberry', 'pitaya']
+    for index, val in enumerate(list1):
+        print('%d: %s' % (index, val))
+
+
+if __name__ == '__main__':
+    main()

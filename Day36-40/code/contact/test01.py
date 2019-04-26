66fb888d23e2094ff8cfdb8c0aeb2af4afee7598
jackfrued <jackfrued@126.com>
2019-04-26 16:29:16 +0800

    更新了数据库部分相关知识

--- Day36-40/code/contact/test01.py
+++
@@ -1,8 +0,0 @@
-def main():
-    list1 = ['orange', 'grape', 'banana', 'waxberry', 'pitaya']
-    for index, val in enumerate(list1):
-        print('%d: %s' % (index, val))
-
-
-if __name__ == '__main__':
-    main()

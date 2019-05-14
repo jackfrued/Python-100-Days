56cccd366d3472f69547093f74b7976a9ed5d43e
唐进民（Guilin Tang） <tjmts@qq.com>
2019-05-14 23:01:54 +0800

    done experiences

---
+++ Day01-15/Day01/code/day01-exp-2.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()

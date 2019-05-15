bf2f95a6ba13b982986c8db2bee06e148da1e949
TangJM <tjmts@qq.com>
2019-05-15 18:12:15 +0800

    更新Day5练习

--- Day61-65/my-exp/day05-exp-01.py
+++ Day61-65/my-exp/day05-exp-01.py
@@ -7,9 +7,9 @@ Date: 20190-5-15
 
 """
 for i in range(100,1000):
-    high = i//100
-    mid = i//10
-    low = i%100
+    high = i // 100
+    mid = i // 10 % 10
+    low = i % 10
     num = high**3+mid**3+low**3
     if i == num:
         print("{}是水仙花数".format(i))

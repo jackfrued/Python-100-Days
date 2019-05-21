12ce2ba99c7f1d9a8d40014ee03e2eacb7699a2e
JalanJiang <448300947@qq.com>
2019-05-21 12:30:33 +0800

    refactor(day03): 依据最新个人所得税法，修改 Day03 练习题中个税免征额为 5000 元

--- Day01-15/Day03/code/tax.py
+++ Day01-15/Day03/code/tax.py
@@ -1,39 +1,39 @@
 """
 输入月收入和五险一金计算个人所得税
-说明：写这段代码时新的个人所得税计算方式还没有颁布
+说明：依据最新个人所得税法，个税免征额为 5000 元
 
-Version: 0.1
+Version: 0.2
 Author: 骆昊
-Date: 2018-02-28
+Date: 2019-05-21
 """
 
 salary = float(input('本月收入: '))
 insurance = float(input('五险一金: '))
-diff = salary - insurance - 3500
+diff = salary - insurance - 5000
 if diff <= 0:
     rate = 0
     deduction = 0
-elif diff < 1500:
+elif diff < 3000:
     rate = 0.03
     deduction = 0
-elif diff < 4500:
+elif diff < 12000:
     rate = 0.1
-    deduction = 105
-elif diff < 9000:
+    deduction = 210
+elif diff < 25000:
     rate = 0.2
-    deduction = 555
+    deduction = 1410
 elif diff < 35000:
     rate = 0.25
-    deduction = 1005
+    deduction = 2660
 elif diff < 55000:
     rate = 0.3
-    deduction = 2755
+    deduction = 4410
 elif diff < 80000:
     rate = 0.35
-    deduction = 5505
+    deduction = 7160
 else:
     rate = 0.45
-    deduction = 13505
+    deduction = 15160
 tax = abs(diff * rate - deduction)
 print('个人所得税: ￥%.2f元' % tax)
-print('实际到手收入: ￥%.2f元' % (diff + 3500 - tax))
+print('实际到手收入: ￥%.2f元' % (diff + 5000 - tax))

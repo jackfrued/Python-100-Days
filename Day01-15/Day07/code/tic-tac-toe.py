8c393abc150c48e7ec1db2f960b6fbe7f81a7f7c
JalanJiang <448300947@qq.com>
2019-05-23 17:46:04 +0800

    feat(day07): 井字棋游戏补充判赢逻辑

--- Day01-15/Day07/code/tic-tac-toe.py
+++ Day01-15/Day07/code/tic-tac-toe.py
@@ -1,9 +1,9 @@
 """
 井字棋游戏
 
-Version: 0.1
+Version: 0.2
 Author: 骆昊
-Date: 2018-03-06
+Date: 2019-05-23
 """
 
 import os
@@ -16,6 +16,24 @@ def print_board(board):
     print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
     print('-+-+-')
     print(board['BL'] + '|' + board['BM'] + '|' + board['BR'])
+
+
+def is_winner(board, turn):
+    """
+    判断走棋者是否是赢家
+
+    :param board: 当前棋盘数据
+    :param turn: 走棋者
+    :return 走棋者是赢家返回 True，否则返回 False
+    """
+    return ((board['TL'] == turn and board['TM'] == turn and board['TR'] == turn) or
+    (board['ML'] == turn and board['MM'] == turn and board['MR'] == turn) or
+    (board['BL'] == turn and board['BM'] == turn and board['BR'] == turn) or
+    (board['TL'] == turn and board['ML'] == turn and board['BL'] == turn) or
+    (board['TM'] == turn and board['MM'] == turn and board['BM'] == turn) or
+    (board['TR'] == turn and board['MR'] == turn and board['BR'] == turn) or
+    (board['TL'] == turn and board['MM'] == turn and board['BR'] == turn) or
+    (board['BL'] == turn and board['MM'] == turn and board['TR'] == turn))
 
 
 def main():
@@ -37,6 +55,9 @@ def main():
             if curr_board[move] == ' ':
                 counter += 1
                 curr_board[move] = turn
+                if is_winner(curr_board, turn):
+                    print("产生赢家：%s 赢得胜利" % turn)
+                    break
                 if turn == 'x':
                     turn = 'o'
                 else:

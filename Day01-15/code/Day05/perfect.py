dd4cd0a778f856c36ce45ac0f0d92f6144dd8542
BlackFlame33 <1411299332@qq.com>
2019-09-24 18:22:35 +0800

    Update perfect.py
    新方法并不需要理解import，逻辑也很简单：就是单纯的把factor遍历到num，若为正因数则加到result里，遍历完后进行比对，是则输出。

--- Day01-15/code/Day05/perfect.py
+++ Day01-15/code/Day05/perfect.py
@@ -7,14 +7,10 @@ Version: 0.1
 Author: 骆昊
 Date: 2018-03-02
 """
-import math
-
 for num in range(1, 10000):
     result = 0
-    for factor in range(1, int(math.sqrt(num)) + 1):
+    for factor in range(1, num):
         if num % factor == 0:
             result += factor
-            if factor > 1 and num // factor != factor:
-                result += num // factor
-    if result == num:
+    if num == result:
         print(num)
